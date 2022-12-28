afa61cbbe421eefd5613b356d3ffbe73b0c1cf17
Juliuuss <j.esp.ver@gmail.com>
2022-12-28 16:36:01 +0000

    final

--- app.js
+++ app.js
@@ -2,5 +2,25 @@ const sum=(a,b) => {
     return a+b
 }
 
-console.log(sum(7,3));
-module.exports={sum};
+let oneEuroIs ={
+    "JPY":127.9,
+    "USD":1.2,
+    "GBP":0.8,
+}
+
+function fromDollarToYen(dolar){
+    let valor_yen= dolar*106.5;
+    return valor_yen;
+}
+
+function fromEuroToDollar(euro){
+    return euro*1.2;
+}
+
+function fromYenToPound(yen){
+    let valor_pound= yen*0.006;
+    return valor_pound;
+}
+
+
+module.exports={sum, fromEuroToDollar,fromDollarToYen, fromYenToPound};
--- test.js
+++ test.js
@@ -1,8 +1,24 @@
-
-const {sum} = require('./app.js');
-
 test('nombre de la funcion' , () => {
+    const {sum} = require('./app.js');
     let total = sum(14,9);
     expect(total).toBe(23);
-    expect(total).not.toBe(22);
 });
+ 
+test('Euro a dolares', function(){
+    const {fromEuroToDollar}=require('./app.js');
+    expect(fromEuroToDollar(3.5)).toBe(4.2);
+})
+
+test('5 dolares a yenes', function(){
+    const {fromDollarToYen}=require('./app.js');
+    let dolar=fromDollarToYen(5);
+    expect(dolar).toBe(532.5);
+
+})
+
+test('13 yenes a pound', function(){
+    const {fromYenToPound}=require('./app.js');
+    let yen=fromYenToPound(13);
+    expect(yen).toBe(0.078);
+
+})
